804162d31b34a1749c086632c7fe9e78308a5a8b
samuelberhane <1samibrhane@gmail.com>
2023-04-03 10:44:18 +0300

    Finish authentication routes and create a realtors app.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
 
-# Create your views here.
+class SignupView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self,request,format=None):
+        data = self.request.data
+        name = data["name"]
+        email = data["email"]
+        password = data["password"]
+        password2 = data["password2"]
+        
+        if (password == password2):
+            if User.objects.filter(email=email).exists():
+                return Response({"error":"Email already exists"})
+            else:
+                if len(password) < 6:
+                    return Response({"error":"Password must be at least 6 characters"})
+                else:
+                    user = User.objects.create_user(email=email,name=name, password=password)
+                    user.save()
+                    return Response({"success":"User Create Successfully."})
+                
+        else:
+            return Response({"error": "Password Don't Match."})
+
+
