c2f705085956905ede9a8fe6db3a00caf8ffb07e
samuelberhane <1samibrhane@gmail.com>
2023-04-03 14:12:06 +0300

    Register listing and add listing url.

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "realtor" ,"is_published", "price",)
+    list_display_links = ("id", "title",)
+    list_filter = ("realtor",)
+    list_editable = ("is_published",)
+    search_fields = ("realtor", "title", "description", "city", "state", "price", "address")  
+    list_per_page = 25
+    
+admin.site.register(Listing, ListingAdmin)
