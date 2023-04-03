375f570a85ca38c5ba41823da5d91dddcaf670b6
samuelberhane <1samibrhane@gmail.com>
2023-04-03 10:58:00 +0300

    Create serializers and views for realtor app.

--- realtors/views.py
+++ realtors/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from .models  import Realtor
+from .serializers import RealtorSerializer
 
-# Create your views here.
+
+class RealtorListView(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    pagination_class = None
+       
+class RealtorView(RetrieveAPIView):
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    
+class TopSellerView(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = Realtor.objects.all(top_seller=True)
+    serializer_class = RealtorSerializer
+    pagination_class = None
+
