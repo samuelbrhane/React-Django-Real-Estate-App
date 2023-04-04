743d48b0a895651bd730f4214ab6b84249347a0a
samuelberhane <1samibrhane@gmail.com>
2023-04-04 09:54:00 +0300

    Create serializers and views of contact app and register it in admin.

--- contacts/views.py
+++ contacts/views.py
@@ -1,1 +1,29 @@
+from rest_framework import permissions
+from rest_framework.views import APIView
+from .models import Contact
+from django.core.mail import send_mail
+from rest_framework.response import Response
+import dotenv
+import os
+dotenv.load_dotenv()
 
+
+class ContactCreateView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self,request,format=None):
+        data = self.request.data
+        try:
+            send_mail(data["subject"], 
+                    "Name: " + data["name"] 
+                    + "\nEmail: " + data["email"]
+                    + "\n\nMessage:\n" + data["message"],
+                    os.getenv('EMAIL_HOST'),
+                    [os.getenv('EMAIL_HOST')],
+                    fail_silently = False)
+            contact = Contact(name=data["name"], email=data["email"], subject=data["subject"], message=data["message"])
+            contact.save()
+            return Response({"success":"Message Send Successfully."})
+        
+        except:
+            return Response({"error":"Message Fail to Send."})
